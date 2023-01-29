32e83a06a600a61908325347569de6a2d0cb9d4b
jini_11 <78905126+jini11@users.noreply.github.com>
2023-01-29 23:19:30 +0900

    [Silver IV] Title: 수열 정렬, Time: 240 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][2];
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i][0] = i;
+			arr[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}
+				return o1[1] - o2[1];
+			}
+		});
+		
+		for (int i = 0; i < n; i++) {
+			result[arr[i][0]] = i;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(result[i] + " ");
+		}
+		
+	}
+
+}
