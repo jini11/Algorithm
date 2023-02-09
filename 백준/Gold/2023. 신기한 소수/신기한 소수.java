e2593a433eb28c24b80e6a0ab4b8fd98b74e0568
jini_11 <78905126+jini11@users.noreply.github.com>
2023-02-09 20:35:01 +0900

    [Gold V] Title: 신기한 소수, Time: 228 ms, Memory: 18276 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-	static int[] first = {2, 3, 5, 7};
-	static int[] other = {1, 3, 5, 7, 9};
+	static int[] first = { 2, 3, 5, 7 };
+	static int[] other = { 1, 3, 5, 7, 9 };
 	static int N;
-	static StringBuilder sb;
+	static StringBuilder answer; // 신기한 소수들
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		sb = new StringBuilder();
+		answer = new StringBuilder();
 		N = sc.nextInt();
 		for (int i = 0; i < first.length; i++) {
-			makePrime(1, Integer.toString(first[i]));
+			makePrime(1, first[i]);
 		}
-		System.out.println(sb.toString());
+		System.out.println(answer.toString());
 	}
-	
-	private static void makePrime(int cnt, String num) {
-		if (cnt == N) {
-			if (isPrime(Integer.parseInt(num))) {
-				sb.append(num + "\n");
-			}
+
+	private static void makePrime(int cnt, int number) {
+		if (!isPrime(number)) {		// 소수가 아닐 경우 종료
 			return;
 		}
-		if (!isPrime(Integer.parseInt(num))) return;
+		if (cnt == N) {		// N번 뽑았을 경우 종료
+			answer.append(number + "\n");
+			return;
+		}
 		for (int i = 0; i < other.length; i++) {
-			makePrime(cnt + 1, num + other[i]);
+			int nextNumber = number * 10 + other[i];
+			makePrime(cnt + 1, nextNumber);
 		}
 	}
-	
+
 	private static boolean isPrime(int num) {
 		for (int i = 2; i <= Math.sqrt(num); i++) {
 			if (num % i == 0) {
