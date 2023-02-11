6c418e314991c75a34f847aaa1d91f8df4cd417d
jini_11 <78905126+jini11@users.noreply.github.com>
2023-02-11 18:43:56 +0900

    [Silver I] Title: 쿼드트리, Time: 304 ms, Memory: 41640 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,46 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] map;
+	static int N;
+	static String[][] map;
 	static StringBuilder sb;
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		
-		for (int i = 0; i < n; i++) {
+		StringTokenizer st;
+
+		N = Integer.parseInt(br.readLine());
+		map = new String[N][N];
+
+		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
-			for (int j = 0; j < n; j++) {
-				map[i][j] = Integer.parseInt(line.split("")[j]);
+			for (int j = 0; j < N; j++) {
+				map[i][j] = line.split("")[j];
 			}
 		}
-		recur(0, 0, n);
-		System.out.println(sb.toString());
+
+		recur(0, 0, N);
+		
+		System.out.println(sb);
 	}
-	private static void recur(int row, int col, int size) {
-		if (check(row, col, size)) {
+
+	private static void recur(int row, int col, int n) {
+		if (checkMap(row, col, n)) {
 			sb.append(map[row][col]);
 			return;
 		}
-		
-		size /= 2;
+
+		n /= 2;
 		sb.append("(");
-		
-		recur(row, col, size);
-		recur(row, col + size, size);
-		recur(row + size, col, size);
-		recur(row + size, col + size, size);
+		recur(row, col, n);
+		recur(row, col + n, n);
+		recur(row + n, col, n);
+		recur(row + n, col + n, n);
 		sb.append(")");
+
 	}
-	private static boolean check(int row, int col, int size) {
-		int video = map[row][col];
-		
-		for (int i = row; i < row + size; i++) {
-			for (int j = col; j < col + size; j++) {
-				if (map[i][j] != video) {
+
+	private static boolean checkMap(int row, int col, int n) {
+		String one = map[row][col];
+		for (int i = row; i < row + n; i++) {
+			for (int j = col; j < col + n; j++) {
+				if (!map[i][j].equals(one)) {
 					return false;
 				}
 			}
