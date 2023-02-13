12e0f65983e8d3f2b8b06cb38cc0bf08b483ecb0
jini_11 <78905126+jini11@users.noreply.github.com>
2023-02-13 21:53:15 +0900

    [Silver II] Title: 좌표 압축, Time: 1832 ms, Memory: 288748 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -3,29 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String []args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] copy = new int[n];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int cnt = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++)
-            arr[i] = copy[i] = Integer.parseInt(st.nextToken());
-        
-        Arrays.sort(copy);
-        for(int i=0;i<n;i++) {
-            if(!map.containsKey(copy[i])) {
-                map.put(copy[i], cnt++);
-            }
-        }
-        for(int i=0;i<n;i++)
-            sb.append(map.get(arr[i])).append(" ");
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int[] arr = new int[N];		// 원본 배열
+		int[] clone = new int[N];	// 정렬할 배열
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = clone[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(clone);	// 오름차순 정렬
+
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {			// 숫자 카운트 해줌
+			if (!map.containsKey(clone[i])) {
+				map.put(clone[i], cnt++);
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			sb.append(map.get(arr[i]) + " ");
+		}
+		System.out.println(sb.toString());
+	}
 }
