840841ad5afb168555904f165ed6618faa99e9f4
jini_11 <78905126+jini11@users.noreply.github.com>
2023-02-27 20:40:27 +0900

    [Silver II] Title: 섬의 개수, Time: 448 ms, Memory: 36304 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int[][] arr;
+    static boolean[][] visited;
+    static int w, h;
+    static int result;
+    static int[] dx = {0, 0, 1, -1, -1, 1, -1, 1};
+    static int[] dy = {1, -1, 0, 0, 1, 1, -1, -1};
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            result = 0;
+            w = sc.nextInt();
+            h = sc.nextInt();
+            if(w==0 && h==0)
+                break;
+            
+            arr = new int[h+1][w+1];
+            visited = new boolean[h+1][w+1];
+
+            for(int i=0;i<h;i++) {
+                for(int j=0;j<w;j++) {
+                    arr[i+1][j+1] = sc.nextInt();
+                }
+            }
+
+            for(int i=1;i<=h;i++) {
+                for(int j=1;j<=w;j++) {
+                    if(arr[i][j]==1 && visited[i][j]==false) {
+                        result++;
+                        bfs(i, j);
+                    }
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.add(new int[] {x, y});
+
+        while(!queue.isEmpty()) {
+            int[] point = queue.poll();
+            for(int i=0;i<8;i++) {
+                int nx = point[0] + dx[i];
+                int ny = point[1] + dy[i];
+                if(nx>=0 && ny>0 && nx<=h && ny<=w) {
+                    if(arr[nx][ny]==1 && visited[nx][ny]==false) {
+                        visited[nx][ny] = true;
+                        queue.add(new int[] {nx, ny});
+                    }
+                }
+            }
+        }
+        
+    }
+}
