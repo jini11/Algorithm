6451831ca2703e9e30d487c71d75a61c54a04ed0
jini_11 <78905126+jini11@users.noreply.github.com>
2023-02-27 20:43:36 +0900

    [Silver II] Title: 섬의 개수, Time: 168 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,63 +1,67 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static boolean[][] visited;
-    static int w, h;
-    static int result;
-    static int[] dx = {0, 0, 1, -1, -1, 1, -1, 1};
-    static int[] dy = {1, -1, 0, 0, 1, 1, -1, -1};
-    public static void main(String []args) {
-        Scanner sc = new Scanner(System.in);
+	static int W, H;
+	static int[][] map;
+	static boolean[][] visited;
+	static int res;
 
-        while(true) {
-            result = 0;
-            w = sc.nextInt();
-            h = sc.nextInt();
-            if(w==0 && h==0)
-                break;
-            
-            arr = new int[h+1][w+1];
-            visited = new boolean[h+1][w+1];
+	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-            for(int i=0;i<h;i++) {
-                for(int j=0;j<w;j++) {
-                    arr[i+1][j+1] = sc.nextInt();
-                }
-            }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 
-            for(int i=1;i<=h;i++) {
-                for(int j=1;j<=w;j++) {
-                    if(arr[i][j]==1 && visited[i][j]==false) {
-                        result++;
-                        bfs(i, j);
-                    }
-                }
-            }
-            System.out.println(result);
-        }
-    }
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			W = Integer.parseInt(st.nextToken());
+			H = Integer.parseInt(st.nextToken());
+			
+			if (W == 0 && H == 0) {
+				break;
+			}
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        visited[x][y] = true;
-        queue.add(new int[] {x, y});
+			map = new int[H][W];
+			visited = new boolean[H][W];
+			res = 0;
+			
+			for (int i = 0; i < H; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < W; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-        while(!queue.isEmpty()) {
-            int[] point = queue.poll();
-            for(int i=0;i<8;i++) {
-                int nx = point[0] + dx[i];
-                int ny = point[1] + dy[i];
-                if(nx>=0 && ny>0 && nx<=h && ny<=w) {
-                    if(arr[nx][ny]==1 && visited[nx][ny]==false) {
-                        visited[nx][ny] = true;
-                        queue.add(new int[] {nx, ny});
-                    }
-                }
-            }
-        }
-        
-    }
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					if (!visited[i][j] && map[i][j] == 1) {
+						dfs(i, j);
+						res++;
+					}
+				}
+			}
+			sb.append(res).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+	private static void dfs(int row, int col) {
+		visited[row][col] = true;
+
+		for (int i = 0; i < 8; i++) {
+			int nr = row + dr[i];
+			int nc = col + dc[i];
+			if (nr < 0 || nr >= H || nc < 0 || nc >= W)
+				continue;
+			if (!visited[nr][nc] && map[nr][nc] == 1) {
+				visited[nr][nc] = true;
+				dfs(nr, nc);
+			}
+		}
+	}
 }
