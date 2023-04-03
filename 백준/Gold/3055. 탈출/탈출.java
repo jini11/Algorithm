4e39731c3bd67e1d563363c4e589302c322b07bf
jini_11 <78905126+jini11@users.noreply.github.com>
2023-04-03 16:29:12 +0900

    [Gold IV] Title: 탈출, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 import java.io.IOException;
@@ -12,7 +11,9 @@ public class Main {
 	static Point start;
 	static int res = Integer.MAX_VALUE;
 	static boolean[][] visitGo;
-	static boolean[][] visitWater;
+
+	static Queue<Point> water = new ArrayDeque<>();
+	
 	static int[] dr = { -1, 0, 1, 0 };
 	static int[] dc = { 0, 1, 0, -1 };
 
@@ -40,10 +41,13 @@ public class Main {
 					start = new Point(i, j, 0);
 					map[i][j] = '.';
 				}
+				if (map[i][j] == '*') {
+					water.add(new Point(i, j, 0));
+				}
 			}
 		}
 		visitGo = new boolean[R][C];
-		visitWater = new boolean[R][C];
+
 		bfs();
 
 		if (res == Integer.MAX_VALUE) {
@@ -58,7 +62,9 @@ public class Main {
 		queue.add(start);
 
 		while (!queue.isEmpty()) {
+
 			waterFall();
+
 			for (int size = 0; size < queue.size(); size++) {
 				Point cur = queue.poll();
 				if (map[cur.row][cur.col] == 'D') {
@@ -69,10 +75,12 @@ public class Main {
 					int nr = cur.row + dr[i];
 					int nc = cur.col + dc[i];
 
-					if (nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == '*' || map[nr][nc] == 'X')
+					if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
 						continue;
-					if (visitGo[nr][nc] || visitWater[nr][nc])
+					}
+					if (map[nr][nc] == '*' || map[nr][nc] == 'X' || visitGo[nr][nc]) {
 						continue;
+					}
 
 					queue.add(new Point(nr, nc, cur.cnt + 1));
 					visitGo[nr][nc] = true;
@@ -82,26 +90,21 @@ public class Main {
 	}
 
 	private static void waterFall() {
-		Queue<Point> water = new ArrayDeque<>();
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == '*') {
-					water.add(new Point(i, j, 0));
-				}
-			}
-		}
-		while (!water.isEmpty()) {
+		int len = water.size();
+		for (int size = 0; size < len; size++) {
 			Point curWater = water.poll();
 			for (int i = 0; i < 4; i++) {
 				int nr = curWater.row + dr[i];
 				int nc = curWater.col + dc[i];
-				if (nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == 'D' || map[nr][nc] == 'X')
+				if (nr < 0 || nr >= R || nc < 0 || nc >= C) {
 					continue;
-				if (visitWater[nr][nc])
+				}
+				if (map[nr][nc] == 'D' || map[nr][nc] == 'X' || map[nr][nc] == '*') {
 					continue;
-				
+				}
+
 				map[nr][nc] = '*';
-				visitWater[nr][nc] = true;
+				water.add(new Point(nr, nc, 0));
 			}
 		}
 	}
