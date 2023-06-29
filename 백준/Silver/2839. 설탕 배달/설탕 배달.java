7636fe6efe46abd874a073fca1a3f216d64ccae8
jini_11 <78905126+jini11@users.noreply.github.com>
2023-06-29 17:00:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+        int n=Integer.parseInt(br.readLine());
+        int count=0;
+
+        while(true){
+            if(n%5==0){
+                count+=n/5;
+                System.out.println(count);
+                break;
+            }
+            else{
+                n-=3;
+                count++;
+            }
+            if(n<0){
+                System.out.println(-1);
+                break;
+            }
+        }
+
+       
+    }
+}
