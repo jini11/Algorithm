b59ce7888cb5cbb0bffadd049f35e35af6f4744a
jini_11 <78905126+jini11@users.noreply.github.com>
2023-08-27 15:37:13 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 632 ms, Memory: 55392 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,81 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int V, E;
-	static int[] parents;
-	static Edge[] edgeList;
-	
-	static class Edge implements Comparable<Edge> {
-		int from, to, weight;
-		public Edge(int from, int to, int weight) {
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
-		}
-		
-	}
-	
-	private static void makeSet() {
-		parents = new int[V+1];
-		for (int i = 1; i <= V; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	private static int findSet(int v) {
-		if (parents[v] == v) {
-			return v;
-		}
-		return parents[v] = findSet(parents[v]);
-	}
-	
-	private static boolean union(int u, int v) {
-		int uRoot = findSet(u);
-		int vRoot = findSet(v);
-		
-		if (uRoot == vRoot) {
-			return false;
-		}
-		
-		parents[uRoot] = vRoot;
-		return true;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		edgeList = new Edge[E];
-		
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			edgeList[i] = new Edge(from, to, weight);
-		}
-		
-		Arrays.sort(edgeList);
-		
-		makeSet();
-		int result = 0, count = 0;
-		
-		for (Edge edge: edgeList) {
-			if (union(edge.from, edge.to)) {
-				result += edge.weight;
-				if (++count == V - 1) break;
-			}
-		}
-		System.out.println(result);
-	}
+    static int V, E;
+
+    static class Node implements Comparable<Node> {
+        int from, to, weight;
+        public Node(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        List<Node>[] adjList = new ArrayList[V + 1];
+        for (int i = 0; i <= V; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            adjList[from].add(new Node(from, to, weight));
+            adjList[to].add(new Node(to, from, weight));
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[] visited = new boolean[V + 1];
+
+        int cnt = 1;
+        int result = 0;
+
+        pq.addAll(adjList[1]);
+        visited[1] = true;
+
+        while (cnt != V) {
+            Node cur = pq.poll();
+            if (visited[cur.to]) {
+                continue;
+            }
+            result += cur.weight;
+            visited[cur.to] = true;
+            pq.addAll(adjList[cur.to]);
+            cnt++;
+        }
+
+        System.out.println(result);
+    }
 }
