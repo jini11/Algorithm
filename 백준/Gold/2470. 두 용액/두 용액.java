afc12dd00450af0fd4a3b9d512c7c8a9a196c8e0
jini_11 <78905126+jini11@users.noreply.github.com>
2023-08-30 08:27:24 +0900

    [Gold V] Title: 두 용액, Time: 392 ms, Memory: 28380 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -5,34 +5,41 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-            
-        Arrays.sort(arr); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        int left = 0;
-        int right = n-1;
-        int max = Integer.MAX_VALUE;
-        int a = 0;
-        int b = 0;
-        while(left < right) {
-            int sum = arr[left] + arr[right];
-            if(Math.abs(sum) < max) {
-                max = Math.abs(sum);
-                a = arr[left];
-                b = arr[right];
-            }
-            if(sum>0)
-                right--;
-            else
-                left++;
-            
-        }
-        System.out.println(a+" "+b);
-    }
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		int left = 0;
+		int right = arr.length - 1;
+		int minLeft = arr[left];
+		int minRight = arr[right];
+		int minSum = Integer.MAX_VALUE;
+
+		while (left < right) {
+			int sum = arr[left] + arr[right];
+			if (Math.abs(minSum) > Math.abs(sum)) {
+				minSum = sum;
+				minLeft = arr[left];
+				minRight = arr[right];
+			}
+
+			if (sum > 0) {
+				right--;
+			} else {
+				left++;
+			}
+		}
+
+		System.out.println(minLeft + " " + minRight);
+	}
 }
