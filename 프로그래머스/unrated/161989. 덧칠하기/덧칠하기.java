3a9ab446f13f509b0439460c6ec01aea22cc7537
jini_11 <78905126+jini11@users.noreply.github.com>
2023-09-10 12:35:04 +0900

    [unrated] Title: 덧칠하기, Time: 0.91 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 1;
+        int start = section[0];
+        
+        for (int i = 1; i < section.length; i++) {
+            if (section[i] < start + m) {
+                continue;
+            }
+            
+            answer++;
+            start = section[i];
+        }
+        
+        return answer;
+    }
+}
