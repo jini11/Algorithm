19ea271fd1f5ae741a58eb3fa62316d4b71acc24
jini_11 <78905126+jini11@users.noreply.github.com>
2024-01-04 09:23:30 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 16288 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -6,61 +6,65 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int M, N;
 	static int[][] map;
-	static int N, M, K, bug;
-	static final int[] dr = {1, -1, 0, 0};
-	static final int[] dc = {0, 0, 1, -1};
-
+	static int[] dr = {1, 0, -1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		
+		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			M = Integer.parseInt(st.nextToken());
 			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-
-			map = new int[N][M];
+			int K = Integer.parseInt(st.nextToken());
+			
+			map = new int[M][N];
+			int cnt = 0;
+			
 			for (int i = 0; i < K; i++) {
 				st = new StringTokenizer(br.readLine());
-				int col = Integer.parseInt(st.nextToken());
 				int row = Integer.parseInt(st.nextToken());
+				int col = Integer.parseInt(st.nextToken());
 				map[row][col] = 1;
 			}
-
-			bug = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j< M; j++) {
-					if (map[i][j] != 0) {
+			
+			for (int i = 0; i < M; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] == 1) {
 						bfs(i, j);
-						bug++;
+						cnt++;
 					}
 				}
 			}
-
-			sb.append(bug + "\n");
+			
+			sb.append(cnt + "\n");
 		}
 		System.out.println(sb.toString());
 	}
-
+	
 	private static void bfs(int row, int col) {
-		Queue<int[]> queue = new ArrayDeque<>();
+		Queue<int[]> queue = new ArrayDeque<int[]>();
 		queue.add(new int[] { row, col });
-
+		
 		while (!queue.isEmpty()) {
 			int[] cur = queue.poll();
 			for (int i = 0; i < 4; i++) {
 				int nr = cur[0] + dr[i];
 				int nc = cur[1] + dc[i];
-
-				if (nr < 0 || nc < 0 || nr >= N || nc >= M || map[nr][nc] == 0) continue;
-
+				
+				if (nr < 0 || nr >= M || nc < 0 || nc >= N || map[nr][nc] == 0) continue;
+				
 				map[nr][nc] = 0;
 				queue.add(new int[] {nr, nc});
+				
 			}
 		}
 	}
+	
 }
