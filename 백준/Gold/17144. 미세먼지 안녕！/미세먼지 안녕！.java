4fdd762bea9b6e247d97bdc3cb878b1b58140cba
jini_11 <78905126+jini11@users.noreply.github.com>
2024-01-08 11:14:33 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 280 ms, Memory: 26584 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,140 +1,157 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int R, C, T;
-	static int[][] map;
-	static int res;
-	static Point aircon;
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, -1, 0, 1 };
-
-	static class Point {
-		int row, col, amount;
-
-		public Point(int row, int col, int amount) {
-			this.row = row;
-			this.col = col;
-			this.amount = amount;
-		}
-	}
-
+	static int R, C, T, dustCnt;
+	static int[][] map, secondMap;
+	static int airconRow, airconCol;
+	
+	static int[] dr = {0, 1, 0, -1};
+	static int[] dc = {1, 0, -1, 0};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-
+		
 		map = new int[R][C];
+		
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < C; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] == -1) {
-					aircon = new Point(i, j, map[i][j]);
+					airconRow = i;
+					airconCol = j;
 				}
 			}
 		}
-
-		for (int time = 0; time < T; time++) {
-			spreadDust();
-			runAircon();
-		}
-
-		System.out.println(getDust());
-	}
-
-	private static void runAircon() {
-		// 위
-		int nr = aircon.row - 2;
-		int nc = 0;
-		int drow = -1;
-		int dcol = 0;
-		while (true) {
-			if (nr == aircon.row - 1 && nc == 1)
-				break;
-			if (nr == aircon.row - 1 && nc == C - 1) {
-				drow = 0;
-				dcol = -1;
-			} else if (nr == 0 && nc == C - 1) {
-				drow = 1;
-				dcol = 0;
-			} else if (nr == 0 && nc == 0) {
-				drow = 0;
-				dcol = 1;
-			}
-
-			map[nr][nc] = map[nr + drow][nc + dcol];
-			nr += drow;
-			nc += dcol;
-		}
-		map[aircon.row - 1][0] = aircon.amount;
-		map[aircon.row - 1][1] = 0;
 		
-		// 아래
-		nr = aircon.row + 1;
-		nc = 0;
-		drow = 1;
-		dcol = 0;
-		while (true) {
-			if (nr == aircon.row && nc == 1)
-				break;
-			if (nr == R - 1 && nc == 0) {
-				drow = 0;
-				dcol = 1;
-			} else if (nr == R - 1 && nc == C - 1) {
-				drow = -1;
-				dcol = 0;
-			} else if (nr == aircon.row && nc == C - 1) {
-				drow = 0;
-				dcol = -1;
-			}
-			map[nr][nc] = map[nr + drow][nc + dcol];
-			nr += drow;
-			nc += dcol;
+		for (int t = 0; t < T; t++) {
+			spreadDust();
+			runAirCon();
 		}
-		map[aircon.row][0] = aircon.amount;
-		map[aircon.row][1] = 0;
+		
+		calcDust();
+		
+		System.out.println(dustCnt);
 	}
-
+	
 	private static void spreadDust() {
-		Queue<Point> queue = new ArrayDeque<>();
+		int[][] secondMap = new int[R][C];
+		
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
-				if (map[i][j] > 0)
-					queue.add(new Point(i, j, map[i][j]));
+				if (map[i][j] > 0) {
+					int spread = bfs(i, j, secondMap);
+					if (spread > 0)
+						map[i][j] -= (map[i][j] / 5 * spread);
+				}
 			}
 		}
-
-		while (!queue.isEmpty()) {
-			Point cur = queue.poll();
-			int cnt = 0;
-			for (int i = 0; i < 4; i++) {
-				int nr = cur.row + dr[i];
-				int nc = cur.col + dc[i];
-
-				if (nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == -1)
-					continue;
-
-				cnt++;
-				map[nr][nc] += cur.amount / 5;
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] == -1) continue;
+				map[i][j] += secondMap[i][j]; 
 			}
-			map[cur.row][cur.col] -= (cur.amount / 5 * cnt);
 		}
 	}
-
-	private static int getDust() {
-		int sum = 0;
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				sum += map[i][j];
+	
+	private static int bfs(int row, int col, int[][] secondMap) {
+		int cnt = 0;
+		for (int i = 0; i < 4; i++) {
+			int nr = row + dr[i];
+			int nc = col + dc[i];
+			
+			if (nr < 0 || nr >= R || nc < 0 || nc >= C || map[nr][nc] == -1) continue;
+			secondMap[nr][nc] += (map[row][col] / 5);
+			cnt++;
+		}
+		return cnt;
+	}
+	
+	private static void runAirCon() {
+		int row = airconRow - 1;
+		int col = airconCol;
+		int rowDir = -1;
+		int colDir = 0;
+		
+		// up
+		while (true) {
+			int nr = row + rowDir;
+			int nc = col + colDir;
+			
+			if (nr == 0 && nc == 0) {
+				rowDir = 0;
+				colDir = 1;
+			} else if (nr == 0 && nc == C - 1) {
+				rowDir = 1;
+				colDir = 0;
+			} else if (nr == airconRow - 1 && nc == C - 1) {
+				rowDir = 0;
+				colDir = -1;
+			}
+			
+			map[row][col] = map[nr][nc];
+			
+			row = nr;
+			col = nc;
+			
+			if (row == airconRow - 1 && col == airconCol) {
+				break;
 			}
 		}
-		return sum + 2;
+		
+		map[airconRow - 1][airconCol] = -1;
+		map[airconRow - 1][airconCol + 1] = 0;
+		
+		// down
+		row = airconRow;
+		col = airconCol;
+		rowDir = 1;
+		colDir = 0;
+		
+		while (true) {
+			int nr = row + rowDir;
+			int nc = col + colDir;
+			
+			if (nr == R - 1 && nc == 0) {
+				rowDir = 0;
+				colDir = 1;
+			} else if (nr == R - 1 && nc == C - 1) {
+				rowDir = -1;
+				colDir = 0;
+			} else if (nr == airconRow && nc == C - 1) {
+				rowDir = 0;
+				colDir = -1;
+			}
+			
+			map[row][col] = map[nr][nc];
+			
+			row = nr;
+			col = nc;
+			
+			if (row == airconRow && col == airconCol) {
+				break;
+			}
+		}
+		
+		map[airconRow][airconCol] = -1;
+		map[airconRow][airconCol + 1] = 0;
+	}
+	
+	private static void calcDust() {
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] == -1) continue;
+				dustCnt += map[i][j];
+			}
+		}
 	}
 }
