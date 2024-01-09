bd2be7d73702dbd8669bc39d5a358d7544ac13ef
jini_11 <78905126+jini11@users.noreply.github.com>
2024-01-09 10:46:12 +0900

    [Gold V] Title: 로봇 청소기, Time: 140 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,48 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m, r, c, d;
-    static int count = 1;
-    static int[][] map;
-    static int[] dx = {-1, 0, 1, 0}; // 북(0) 동(1) 남(2) 서(3)
-    static int[] dy = {0, 1, 0, -1};
-    public static void main(String []args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        map = new int[n][m];
-        r = sc.nextInt(); // 청소기 x
-        c = sc.nextInt(); // 청소기 y
-        d = sc.nextInt(); // 방향
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                map[i][j] = sc.nextInt();
-            }
-        }
-        dfs(r, c, d);
-        System.out.println(count);
-    }
-    public static void dfs(int x, int y, int d) {
-        map[x][y] = 2; // 청소함
-
-        for(int i=0;i<4;i++) {
-            d -= 1;
-            if(d==-1) d=3;
-
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-            if(map[nx][ny] == 0) {
-                count++;
-                dfs(nx, ny, d);
-                return;
-            }
-        }
-        //주변에 청소할 공간이 없는 경우
-        int dd = (d + 2) % 4;
-        int mx = x + dx[dd];
-        int my = y + dy[dd];
-        if(map[mx][my] != 1 && mx>=0 && my>=0 && mx<n && my<m)
-            dfs(mx, my, d);
-    }
+	
+	static int N, M, r, c, d, cnt;
+	static int[][] map;
+	
+	static int[] dr = {-1, 0, 1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dfs(r, c, d);
+		
+		System.out.println(cnt + 1);
+	}
+	
+	private static void dfs(int row, int col, int dir) {
+		map[row][col] = 2;
+		
+		// 청소할 칸이 있는 경우
+		for (int i = 0; i < 4; i++) {
+			dir -= 1;
+			if (dir == -1) dir = 3;
+			
+			int nr = row + dr[dir];
+			int nc = col + dc[dir];
+			
+			if (nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 1) continue;
+			if (map[nr][nc] == 0) {
+				cnt++;
+				dfs(nr, nc, dir);
+				return;
+			}
+		}
+		
+		// 청소할 칸이 없는 경우
+		int backDir = (dir + 2) % 4;
+		int nr = row + dr[backDir];
+		int nc = col + dc[backDir];
+		if (nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 1) return;
+		dfs(nr, nc, dir);
+		
+	}
 }
