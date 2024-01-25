fb8ede90f8ddfb09b7768e54400961691a4066c6
jini_11 <78905126+jini11@users.noreply.github.com>
2024-01-25 10:33:08 +0900

    [Gold V] Title: 두 용액, Time: 368 ms, Memory: 29124 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -5,41 +5,38 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
 		int N = Integer.parseInt(br.readLine());
-
 		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
+		int res = Integer.MAX_VALUE;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-
+		
 		Arrays.sort(arr);
-
+		
 		int left = 0;
-		int right = arr.length - 1;
-		int minLeft = arr[left];
-		int minRight = arr[right];
-		int minSum = Integer.MAX_VALUE;
-
+		int right = N - 1;
+		int minLeft = 0;
+		int minRight = 0;
 		while (left < right) {
 			int sum = arr[left] + arr[right];
-			if (Math.abs(minSum) > Math.abs(sum)) {
-				minSum = sum;
+			if (res > Math.abs(sum)) {
+				res = Math.abs(sum);
 				minLeft = arr[left];
 				minRight = arr[right];
 			}
-
-			if (sum > 0) {
-				right--;
+			if (sum < 0) {
+				left += 1;
 			} else {
-				left++;
+				right -= 1;
 			}
 		}
-
+		
 		System.out.println(minLeft + " " + minRight);
 	}
 }
