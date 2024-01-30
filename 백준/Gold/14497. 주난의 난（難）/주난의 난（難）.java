5b4b08faa60b73b0863cc3ec9f3c9253a7fbc6e0
jini_11 <78905126+jini11@users.noreply.github.com>
2024-01-30 13:59:32 +0900

    [Gold IV] Title: 주난의 난(難), Time: 224 ms, Memory: 21228 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -2,94 +2,78 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;	
 
 public class Main {
-	
+
 	static int N, M, res;
 	static int[][] map;
-	static List<int[]> emptyList;
 	static int startRow, startCol, endRow, endCol;
-	static int[] dr = {1, 0, -1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	
+	static int[] dr = { 1, 0, -1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+
 		st = new StringTokenizer(br.readLine());
 		startRow = Integer.parseInt(st.nextToken()) - 1;
 		startCol = Integer.parseInt(st.nextToken()) - 1;
 		endRow = Integer.parseInt(st.nextToken()) - 1;
 		endCol = Integer.parseInt(st.nextToken()) - 1;
-		
+
 		map = new int[N][M];
-		emptyList = new ArrayList<int[]>();
-		
+
 		for (int i = 0; i < N; i++) {
 			String line = br.readLine();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = line.charAt(j) - '0';
-				if (map[i][j] == -13 || map[i][j] == -6) {
+				char input = line.charAt(j);
+				if (input == '1') {
+					map[i][j] = 1;
+				} else {
 					map[i][j] = 0;
 				}
 			}
 		}
-		
-		while (true) {
-			if (findChoco()) {
-				break;
-			}
-			wave();
-			res++;
-		}
-		System.out.println(res + 1);
+		System.out.println(bfs());
 	}
-	
-	private static boolean findChoco() {
-		Queue<int[]> queue = new ArrayDeque<int[]>();
-		boolean[][] visited = new boolean[N][M];
-		queue.add(new int[] { startRow, startCol});
-		emptyList.add(new int[] { startRow, startCol });
-		visited[startRow][startCol] = true;
-		
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			if (cur[0] == endRow && cur[1] == endCol) {
-				return true;
-			}
+
+	private static int bfs() {
+		Deque<int[]> deque = new ArrayDeque<int[]>();
+		int[][] countBoard = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			Arrays.fill(countBoard[i], -1);
+		}
+		countBoard[startRow][startCol] = 0;
+		deque.add(new int[] { startRow, startCol });
+
+		while (!deque.isEmpty()) {
+			int[] cur = deque.poll();
+			int cnt = countBoard[cur[0]][cur[1]];
+
 			for (int i = 0; i < 4; i++) {
 				int nr = cur[0] + dr[i];
 				int nc = cur[1] + dc[i];
-				if (nr < 0 || nc < 0 || nr >= N || nc >= M || visited[nr][nc]) continue;
-				if (map[nr][nc] == 0) {
-					emptyList.add(new int[] { nr, nc });
-					queue.add(new int[] {nr, nc});
-					visited[nr][nc] = true;
-				}
-			}
-		}
-		
-		return false;
-	}
-	
-	private static void wave() {
-		for (int[] empty : emptyList) {
-			for (int i = 0; i < 4; i++) {
-				int nr = empty[0] + dr[i];
-				int nc = empty[1] + dc[i];
-				
-				if (nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
-				if (map[nr][nc] == 1) {
-					map[nr][nc] = 0;
+
+				if (nr < 0 || nr >= N || nc < 0 || nc >= M)
 					continue;
+				if (countBoard[nr][nc] != -1)
+					continue;
+				if (map[nr][nc] == 1) {
+					countBoard[nr][nc] = cnt + 1;
+					deque.add(new int[] { nr, nc });
+				} else if (map[nr][nc] == 0) {
+					countBoard[nr][nc] = cnt;
+					deque.addFirst(new int[] { nr, nc });
 				}
 			}
 		}
+
+		res = countBoard[endRow][endCol] + 1;
+		return res;
 	}
 }
