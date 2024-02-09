9404c45c67993ffb56f8b97ca4f673e1873ccd0f
jini_11 <78905126+jini11@users.noreply.github.com>
2024-02-09 10:38:39 +0900

    [Gold II] Title: 물대기, Time: 392 ms, Memory: 27140 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.java
+++ 백준/Gold/1368. 물대기/물대기.java
@@ -1,92 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int[] parents;
-	static List<Edge> edgeList;
-
-	static class Edge implements Comparable<Edge> {
-		int from, to, weight;
-
-		public Edge(int from, int to, int weight) {
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
+	
+	static class Node implements Comparable<Node> {
+		int idx, w;
+		public Node (int idx, int w) {
+			this.idx = idx;
+			this.w = w;
 		}
-
 		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.weight, o.weight);
+		public int compareTo(Node o) {
+			return this.w - o.w;
 		}
 	}
-
-	private static void makeSet() {
-		parents = new int[N + 1];
-		for (int i = 0; i <= N; i++) {
-			parents[i] = i;
-		}
-	}
-
-	private static int findSet(int v) {
-		if (parents[v] == v) {
-			return v;
-		}
-		return parents[v] = findSet(parents[v]);
-	}
-
-	private static boolean union(int u, int v) {
-		int uRoot = findSet(u);
-		int vRoot = findSet(v);
-
-		if (uRoot == vRoot) {
-			return false;
-		}
-		parents[uRoot] = vRoot;
-		return true;
-	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		int[] well = new int[N + 1];
-		edgeList = new ArrayList<>();
-		long res = 0;
-		for (int i = 1; i <= N; i++) {
-			well[i] = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int w = Integer.parseInt(br.readLine());
+			pq.add(new Node(i, w));
 		}
 
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
-				int weight = Integer.parseInt(st.nextToken());
-				if (i != j) {
-					edgeList.add(new Edge(i, j, weight));
-				} else {
-					edgeList.add(new Edge(0, i, well[i]));
-				}
+		
+		int[][] adjMatrix = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				adjMatrix[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		Collections.sort(edgeList);
-
-		makeSet();
-
-		int cnt = 0;
-		for (Edge edge : edgeList) {
-			if (union(edge.from, edge.to)) {
-				res += edge.weight;
-				if (++cnt == N) {
-					break;
-				}
+		
+		int res = 0;
+		boolean[] visited = new boolean[N];
+		
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			if (visited[cur.idx]) continue;
+			
+			visited[cur.idx] = true;
+			res += cur.w;
+			
+			for (int i = 0; i < N; i++) {
+				if (i == cur.idx) continue;
+				pq.add(new Node(i, adjMatrix[cur.idx][i]));
 			}
 		}
+		
 		System.out.println(res);
 	}
 }
