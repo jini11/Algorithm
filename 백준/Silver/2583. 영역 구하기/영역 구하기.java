db59113b4b1be7532b68986cdd77d5922c502ae2
jini_11 <78905126+jini11@users.noreply.github.com>
2024-03-06 23:06:54 +0900

    [Silver I] Title: 영역 구하기, Time: 140 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,66 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
-    static boolean[][] visited;
-    static int m, n;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int count;
-    public static void main(String []args) {
-        Scanner sc = new Scanner(System.in);
-
-        m = sc.nextInt();
-        n = sc.nextInt();
-        int k = sc.nextInt();
-
-        arr = new int[n][m];
-        visited = new boolean[n][m];
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while(k!=0) {
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-            
-            for(int i=x1;i<x2;i++) {
-                for(int j=y1;j<y2;j++) {
-                    arr[i][j] = 1;
-                }
-            }
-            k--;
-        }
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(arr[i][j]==0 && visited[i][j]==false) {
-                    count = 0;
-                    dfs(i, j);
-                    list.add(count);
-                }
-            }
-        }
-        Collections.sort(list);        
-        System.out.println(list.size());
-        for(int i=0;i<list.size();i++)
-            System.out.print(list.get(i)+" ");
-    }
-
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;
-        count++;
-        for(int i=0;i<4;i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx>=0 && ny>=0 && nx<n && ny<m) {
-                if(arr[nx][ny]==0 && visited[nx][ny]==false) {
-                    dfs(nx, ny);
-                }
-            }
-        }
-    }
+	
+	static int M, N, K, cnt;
+	static boolean[][] map;
+	static List<Integer> res;
+	
+	static int[] dr = {1, 0, -1, 0};
+	static int[] dc = {0, 1, 0, -1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		map = new boolean[N][M];
+		res = new ArrayList<Integer>();
+		
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int startRow = Integer.parseInt(st.nextToken());
+			int startCol = Integer.parseInt(st.nextToken());
+			int endRow = Integer.parseInt(st.nextToken());
+			int endCol = Integer.parseInt(st.nextToken());
+			drawSquare(startRow, startCol, endRow, endCol);
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (!map[i][j]) {
+					res.add(bfs(i, j));
+				}
+			}
+		}
+		
+		Collections.sort(res);
+		System.out.println(res.size());
+		for (Integer sum : res) {
+			System.out.print(sum + " ");
+		}
+	}
+	
+	private static void drawSquare(int startRow, int startCol, int endRow, int endCol) {
+		for (int i = startRow; i < endRow; i++) {
+			for (int j = startCol; j < endCol; j++) {
+				map[i][j] = true;
+			}
+		}
+	}
+	
+	private static int bfs(int row, int col) {
+		Queue<int[]> queue = new ArrayDeque<int[]>();
+		queue.add(new int[] {row, col});
+		map[row][col] = true;
+		int sum = 1;
+		
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = cur[0] + dr[i];
+				int nc = cur[1] + dc[i];
+				if (nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc]) continue;
+				
+				queue.add(new int[] {nr, nc});
+				map[nr][nc] = true;
+				sum++;
+			}
+		}
+		
+		return sum;
+	}
 }
