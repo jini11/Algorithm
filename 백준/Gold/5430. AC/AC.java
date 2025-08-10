7bdfd5568f01039cd5a8575e593fdd1cb8a38068
jini_11 <78905126+jini11@users.noreply.github.com>
2025-08-10 18:04:49 +0900

    [Gold V] Title: AC, Time: 2120 ms, Memory: 135044 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,71 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
-        for (int t = 0; t < T; t++) {
+        while (T-- > 0) {
             String command = br.readLine();
-            command = command.replaceAll("RR", "");
+            int N = Integer.parseInt(br.readLine());
             char[] commands = command.toCharArray();
-            int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
             List<Integer> list = new ArrayList<>();
             while (st.hasMoreTokens()) {
                 list.add(Integer.parseInt(st.nextToken()));
             }
 
-            boolean rotate = false;
-            boolean isDelete = false;
-            for (int i = 0; i < commands.length; i++) {
-                if (commands[i] == 'R') {
-                    rotate = !rotate;
+            StringBuilder sb = new StringBuilder("[");
+            String[] com = command.split("");
+            boolean dir = true;
+            for (int i = 0; i < com.length; i++) {
+                if (com[i].equals("R")) {
+                    dir = !dir;
                 } else {
                     if (list.size() == 0) {
-                        isDelete = true;
+                        sb = new StringBuilder("error");
                         break;
                     }
-                    if (!rotate) {
+                    if (dir) {
                         list.remove(0);
                     } else {
                         list.remove(list.size() - 1);
                     }
                 }
             }
-            if (list.size() > 0) {
-                sb.append(listToString(list, rotate) + "\n");
+            if (sb.toString().equals("error")) {
+                System.out.println(sb);
+                continue;
+            }
+            if (dir) {
+                for (Integer n : list) {
+                    sb.append(n).append(",");
+                }
             } else {
-                if (isDelete) {
-                    sb.append("error\n");
-                } else {
-                    sb.append("[]\n");
+                for (int i = list.size() - 1; i >= 0; i--) {
+                    sb.append(list.get(i)).append(",");
                 }
             }
-        }
-        System.out.println(sb.toString());
-    }
-
-    public static String listToString(List<Integer> list, boolean rotate) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        if (!rotate) {
-            for (int i = 0; i < list.size(); i++) {
-                sb.append(list.get(i)).append(",");
+            if (sb.toString().length() > 1) {
+                sb = sb.delete(sb.length() - 1, sb.length());
             }
-        } else {
-            for (int i = list.size() - 1; i >= 0; i--) {
-                sb.append(list.get(i)).append(",");
-            }
+            sb.append("]");
+            System.out.println(sb);
         }
-        sb.delete(sb.length()-1, sb.length());
-        sb.append("]");
-        return sb.toString();
     }
 }
