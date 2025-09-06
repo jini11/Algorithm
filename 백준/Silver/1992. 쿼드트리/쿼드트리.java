cf3f43735bc5bcca0004fc354bb1c798f683b8fe
jini_11 <78905126+jini11@users.noreply.github.com>
2025-09-06 20:49:03 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -3,54 +3,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int N;
-	static int[][] map;
-	static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < N; j++) {
-				map[i][j] = line.charAt(j) - '0';
-			}
-		}
+    static int N;
+    static char[][] map;
+    static StringBuilder sb;
 
-		sb = new StringBuilder();
-		recur(0, 0, N);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder('(');
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            map[i] = line.toCharArray();
+        }
 
-		System.out.println(sb.toString());
-	}
+        recur(0, 0, N);
 
-	private static void recur(int row, int col, int range) {
-		if (checkArea(row, col, range)) {
-			sb.append(map[row][col]);
-			return;
-		}
+        System.out.println(sb);
+    }
 
-		sb.append("(");
+    private static void recur(int r, int c, int len) {
+        if (isSame(r, c, len)) {
+            sb.append(map[r][c]);
+        } else {
+            sb.append("(");
+            int next = len / 2;
+            recur(r, c, next);
+            recur(r, c + next , next);
+            recur(r + next, c, next);
+            recur(r + next, c + next, next);
+            sb.append(")");
+        }
+    }
 
-		int half = range / 2;
-		recur(row, col, half);
-		recur(row, col + half, half);
-		recur(row + half, col, half);
-		recur(row + half, col + half, half);
-
-		sb.append(")");
-	}
-
-	private static boolean checkArea(int row, int col, int range) {
-		int check = map[row][col];
-		for (int i = row; i < row + range; i++) {
-			for (int j = col; j < col + range; j++) {
-				if (map[i][j] != check) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    private static boolean isSame(int r, int c, int len) {
+        char num = map[r][c];
+        for (int i = r; i < r + len; i++) {
+            for (int j = c; j < c + len; j++) {
+                if (map[i][j] != num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
-
