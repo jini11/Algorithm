cbec63a816117acdc7985ba82c355c725ceb4084
jini_11 <78905126+jini11@users.noreply.github.com>
2025-10-11 21:33:36 +0900

    [Gold V] Title: 조 짜기, Time: 132 ms, Memory: 15100 KB -BaekjoonHub

---
+++ 백준/Gold/2229. 조 짜기/조 짜기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] score = new int[N + 1];
+        int[] dp = new int[N + 1];
+        int max = 0;
+        for (int i = 1; i < N + 1; i++) {
+            score[i] = Integer.parseInt(st.nextToken());
+            for (int j = i - 1; j >= 1; j--) {
+                max = Math.max(max, Math.abs(score[i] - score[j]) + dp[j - 1]);
+            }
+            dp[i] = max;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
