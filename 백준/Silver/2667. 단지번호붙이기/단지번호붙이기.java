8295cf6ae6d80e606dea88245d7617fcde7bf156
jini_11 <78905126+jini11@users.noreply.github.com>
2026-01-12 21:12:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,58 +1,66 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int[][] arr;
+
+    static int N;
+    static char[][] map;
     static boolean[][] visited;
-    static int result;
-    static int n;
-    static int[] xx = {1, -1, 0, 0};
-    static int[] yy = {0, 0, 1, -1};
-    public static void main(String []args) throws IOException {
+    static List<Integer> house;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(br.readLine());
-
-        arr = new int[n][n];
-        visited = new boolean[n][n];
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i=0;i<n;i++) {
-            String temp = br.readLine();
-            for(int j=0;j<n;j++)
-                arr[i][j] = temp.charAt(j) - '0';
-        }
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(arr[i][j]==1 && visited[i][j]==false) {
-                    result = 1;
-                    dfs(i,j);
-                    list.add(result);
-                }
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j);
             }
         }
 
-        Collections.sort(list);
-        System.out.println(list.size());
-        for(int i=0;i<list.size();i++)
-            System.out.println(list.get(i));
+        house = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == '0' || visited[i][j]) continue;
+                house.add(bfs(i, j));
+            }
+        }
 
+        Collections.sort(house);
+        System.out.println(house.size());
+        for (int num : house) {
+            System.out.println(num);
+        }
     }
-    public static void dfs(int x, int y) {
-        visited[x][y] = true;
 
-        for(int i=0;i<4;i++) {
-            int nx = x + xx[i];
-            int ny = y + yy[i];
+    private static int bfs(int r, int c) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        visited[r][c] = true;
+        queue.add(new int[] {r, c});
 
-            if(nx>=0 && ny>=0 && nx<n && ny<n) {
-                if(arr[nx][ny]==1 && visited[nx][ny]==false) {
-                    dfs(nx, ny);
-                    result++;
-                }
+        int[] dr = {1, 0, -1, 0};
+        int[] dc = {0, 1, 0, -1};
+        int houseCnt = 1;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
+                if (isOut(nr, nc) || visited[nr][nc] || map[nr][nc] == '0') continue;
+                visited[nr][nc] = true;
+                queue.add(new int[] {nr, nc});
+                houseCnt++;
             }
         }
+        return houseCnt;
+    }
+
+    private static boolean isOut(int nr, int nc) {
+        return nr < 0 || nc < 0 || nr >= N || nc >= N;
     }
 }
