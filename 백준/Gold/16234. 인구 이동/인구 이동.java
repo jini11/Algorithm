4d7527826ec39ddd290f90ac24ff5d154f65ad31
jini_11 <78905126+jini11@users.noreply.github.com>
2026-03-22 22:00:30 +0900

    [Gold IV] Title: 인구 이동, Time: 696 ms, Memory: 299196 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,117 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	static int N, L, R;
-	static int[][] map;
-	static boolean[][] visited;
-	static int res;
-	
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	
-	static class Point {
-		int row, col;
-		public Point(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][N];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		while (true) {
-			if (!movePeople()) {
-				break;
-			}
-			res++;
-		}
-		
-		System.out.println(res);
-	}
-	
-	private static boolean movePeople() {
-		visited = new boolean[N][N];
-		int cnt = 0;
-		int go = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				List<Point> people = new ArrayList<>();
-				if (!visited[i][j]) {
-					go = bfs(i, j, people);
-					if (go == 0) {
-						cnt++;
-					} else {
-						people.add(new Point(i, j));
-						moveNow(people, go);
-					}
-				}
-			}
-		}
-		if (cnt == N * N) {
-			return false;
-		}
-		
-		return true;
-	}
-	
-	private static int bfs(int row, int col, List<Point> people) {
-		Queue<Point> queue = new ArrayDeque<>();
-		queue.add(new Point(row, col));
-		visited[row][col] = true;
-		int sum = map[row][col];
-		
-		while(!queue.isEmpty()) {
-			Point cur = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int nr = cur.row + dr[i];
-				int nc = cur.col + dc[i];
-				int num = map[cur.row][cur.col];
-				
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N || visited[nr][nc]) {
-					continue;
-				}
-			
-				int distance = Math.abs(num - map[nr][nc]);
-				if (distance >= L && distance <= R) {
-					queue.offer(new Point(nr, nc));
-					visited[nr][nc] = true;
-					people.add(new Point(nr, nc));
-					sum += map[nr][nc];
-				}
-			}
-		}
-		
-		if (people.size() == 0) {
-			return 0;
-		}
-		return sum;
-	}
-	
-	private static void moveNow(List<Point> people, int go) {
-		int avg = go / people.size();
-		for (Point person : people) {
-			map[person.row][person.col] = avg;
-		}
-	}
+
+    static int N, L, R, time;
+    static boolean[][] visited;
+    static int[][] map;
+    static int[] dr = {1, 0, -1, 0};
+    static int[] dc = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        while (true) {
+            boolean isContinue = false;
+            visited = new boolean[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (visited[i][j]) continue;
+                    if (bfs(i, j)) {
+                        isContinue = true;
+                    }
+                }
+            }
+            if (!isContinue) break;
+            time++;
+        }
+
+        System.out.println(time);
+    }
+
+    private static boolean bfs(int r, int c) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[] {r, c});
+        visited[r][c] = true;
+        int peopleCnt = map[r][c];
+        queue.add(new int[] {r, c}); // r, c, 칸의 개수, 인구수
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nr = cur[0] + dr[i];
+                int nc = cur[1] + dc[i];
+                if (isOut(nr, nc) || visited[nr][nc]) continue;
+                int diff = Math.abs(map[cur[0]][cur[1]] - map[nr][nc]);
+                if (diff >= L && diff <= R) {
+                    peopleCnt += map[nr][nc];
+                    list.add(new int[] {nr, nc});
+                    queue.add(new int[] {nr, nc});
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+        if (list.size() > 1) {
+            int movedCnt = peopleCnt / list.size();
+            for (int[] tmp : list) {
+                map[tmp[0]][tmp[1]] = movedCnt;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOut(int nr, int nc) {
+        return nr < 0 || nc < 0 || nr >= N || nc >= N;
+    }
 }
